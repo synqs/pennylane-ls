f85a809b6e1c74cbddca09923acc5a3c89e9a437
manrud <manuel.rudolph@kip.uni-heidelberg.de>
2020-04-27 11:55:57 +0200

    some renaming

--- pennylane-ls/__init__.py
+++
@@ -1,3 +0,0 @@
-from .SoPaExperiment import SoPaExperiment
-from .SoPaOps import LoadMOT, Id
-from ._version import __version__
--- pennylane-ls/_version.py
+++
@@ -1,5 +0,0 @@
-"""Version information.
-   Version number (major.minor.patch[-label])
-"""
-
-__version__ = "0.0.1[-dev]"
