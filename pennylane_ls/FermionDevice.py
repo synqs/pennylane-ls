c08aaf49b06b26fa4b8a904de5b04b5cd9485888
cvjjm <christian.gogolin@covestro.com>
2021-07-29 15:35:11 +0000

    added PauliZ and HartreeFock operations map existing operations to alternative names upper cased all operation classes calculate samples only once by moving the code to pre_measure() and store samples in self._samples

--- pennylane_ls/FermionDevice.py
+++ pennylane_ls/FermionDevice.py
@@ -2,14 +2,14 @@
 import numpy as np
 import scipy
 
-from pennylane import Device
+from pennylane import Device, DeviceError
 from pennylane.operation import Observable
 
 # observables
 from .FermionOps import ParticleNumber
 
 # operations
-from .FermionOps import load, hop, inter, phase
+from .FermionOps import Load, HartreeFock, Hop, Inter, Phase, PauliZ
 
 # classes
 from .FermionOps import FermionObservable, FermionOperation
@@ -21,31 +21,39 @@ import json
 class FermionDevice(Device):
     ## Define operation map for the experiment
     _operation_map = {
-        'load': load,
-        'hop': hop,
-        'inter':inter,
-        'phase':phase,
+        'Load': Load,
+        'Hop': Hop,
+        'Tunneling': Hop,
+        'Inter':Inter,
+        'OnSiteInteraction':Inter,
+        'Phase':Phase,
+        'ChemicalPotential':Phase,
+        'HartreeFock':HartreeFock
     }
 
     name = "Fermion Quantum Simulator Simulator plugin"
     pennylane_requires = ">=0.16.0"
-    version = '0.0.1'
+    version = '0.2.0'
     author = "Vladimir and Donald"
 
-    short_name = "synqs.fqe"
+    short_name = "synqs.ls"
 
     _observable_map = {
         'ParticleNumber': ParticleNumber,
+        'PauliZ': PauliZ,
     }
 
-    def __init__(self, shots=1, username = None, password = None):
+    def __init__(self, wires=8, shots=1, username=None, password=None):
         """
         The initial part.
         """
-        super().__init__(wires=8,shots=shots)
+        if not wires <= 8:
+            raise ValueError()
+        super().__init__(wires=wires,shots=shots)
         self.username = username
         self.password = password
         self.url_prefix = "http://qsimsim.synqs.org/fermions/"
+        self._samples = None
 
     def pre_apply(self):
         self.reset()
@@ -64,31 +72,33 @@ class FermionDevice(Device):
         # check with different operations
         operation_class = self._operation_map[operation]
         if issubclass(operation_class, FermionOperation):
-            l_obj = operation_class.fermion_operator(wires,par)
-
-            self.job_payload['experiment_0']['instructions'].append(l_obj)
+            l_obj = operation_class.fermion_operator(wires, par)
+            if not isinstance(l_obj, list):
+                l_obj = [l_obj]
+            for l_obj_element in l_obj:
+                self.job_payload['experiment_0']['instructions'].append(l_obj_element)
         else:
             raise NotImplementedError()
 
-    def expval(self,  observable=None, wires=None, par=None):
+    def expval(self, observable=None, wires=None, par=None):
         """
         Retrieve the requested observable expectation value.
         """
-        try:
-            shots = self.sample(observable, wires, par)
-            return np.mean(shots, axis=0)
-        except:
-            raise NotImplementedError()
+        shots = self.sample(observable, wires, par)
+        if self._observable_map[observable] == PauliZ:
+            shots = np.ones(shots.shape) - 2*shots
+        mean = np.mean(shots, axis=0)
+        return sum(mean[wires.tolist()])
 
     def var(self, observable=None, wires=None, par=None):
         """
         Retrieve the requested observable variance.
         """
-        try:
-            shots = self.sample(observable, wires, par)
-            return np.var(shots, axis=0)
-        except:
-            raise NotImplementedError()
+        shots = self.sample(observable, wires, par)
+        if self._observable_map[observable] == PauliZ:
+            shots = np.ones(shots.shape) - 2*shots
+        var = np.var(shots, axis=0)
+        return sum(var[wires.tolist()])
 
     def sample(self, observable, wires, par):
         """
@@ -96,34 +106,39 @@ class FermionDevice(Device):
         """
         observable_class = self._observable_map[observable]
         if issubclass(observable_class, FermionObservable):
-            # submit the job
-            wires = wires.tolist()
-            for wire in wires:
-                m_obj = ('measure', [wire], [])
-                self.job_payload['experiment_0']['instructions'].append(m_obj)
-            url= self.url_prefix + "post_job/"
-            job_response = requests.post(url, data={'json':json.dumps(self.job_payload),
-                                                         'username': self.username,'password':self.password})
-
-            job_id = (job_response.json())['job_id']
-
-            # obtain the job result
-            result_payload = {'job_id': job_id}
-            url= self.url_prefix + "get_job_result/"
-
-            result_response = requests.get(url, params={'json':json.dumps(result_payload),
-                                                        'username': self.username,'password':self.password})
-            results_dict = json.loads(result_response.text)
-            results = results_dict["results"][0]['data']['memory']
-
-            num_obs = len(wires)
-            out = np.zeros((self.shots,num_obs))
-            for i1 in np.arange(self.shots):
-                temp = results[i1].split()
-                for i2 in np.arange(num_obs):
-                    out[i1,i2] = int(temp[i2])
-            return out
+            return self._samples
         raise NotImplementedError()
+
+    def pre_measure(self):
+        # submit the job
+        wires = self.wires
+        for wire in wires:
+            m_obj = ('measure', [wire], [])
+            self.job_payload['experiment_0']['instructions'].append(m_obj)
+        url= self.url_prefix + "post_job/"
+        job_response = requests.post(url, data={'json':json.dumps(self.job_payload),
+                                                     'username': self.username,'password':self.password})
+
+        job_id = (job_response.json())['job_id']
+
+        # obtain the job result
+        result_payload = {'job_id': job_id}
+        url= self.url_prefix + "get_job_result/"
+
+        result_response = requests.get(url, params={'json':json.dumps(result_payload),
+                                                    'username': self.username,'password':self.password})
+        results_dict = json.loads(result_response.text)
+        if "results" not in results_dict:
+            raise DeviceError(result_response.text)
+        results = results_dict["results"][0]['data']['memory']
+
+        num_obs = len(wires)
+        out = np.zeros((self.shots,num_obs))
+        for i1 in np.arange(self.shots):
+            temp = results[i1].split()
+            for i2 in np.arange(num_obs):
+                out[i1,i2] = int(temp[i2])
+        self._samples = out
 
     @property
     def operations(self):
@@ -134,4 +149,4 @@ class FermionDevice(Device):
         return set(self._observable_map.keys())
 
     def reset(self):
-        pass
+        self._samples = None
