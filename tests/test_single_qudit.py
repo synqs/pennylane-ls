269bfbd16cb6dd60a2af9aa780d5f35596c82134
Fred Jendrzejewski <fred.jendrzejewski@gmail.com>
2021-10-20 09:05:26 +0200

    Attempt to get the tests for the single_qudit device

--- tests/test_single_qudit.py
+++ tests/test_single_qudit.py
@@ -7,3 +7,12 @@ class TestSingleQuditDevice(unittest.TestCase):
     def test_creation(self):
         testDevice = qml.device("synqs.sqs")
         self.assertEqual(testDevice.operations, {'load', 'rLx', 'rLz', 'rLz2'})
+
+    def test_creation_with_user(self):
+        '''
+        Test creation with username
+        '''
+        username = 'synqs_test'
+        password = 'Cm2TXfRmXennMQ5'
+        testDevice = qml.device("synqs.sqs", shots = 50, username = username, password = password, blocking=True)
+        self.assertEqual(testDevice.operations, {'load', 'rLx', 'rLz', 'rLz2'})
