9cd31baad29130ae76ffaceb11f949eb475b074e
Fred Jendrzejewski <fred.jendrzejewski@gmail.com>
2021-10-21 09:51:05 +0200

    Black the code

--- heroku_credentials.py
+++ heroku_credentials.py
@@ -1,4 +1,4 @@
 # each user has his own credentials file. Do not share this with other users.
 
-username = 'synqs_test' #Put here your username
-password = 'Cm2TXfRmXennMQ5' #and the pwd
+username = "synqs_test"  # Put here your username
+password = "Cm2TXfRmXennMQ5"  # and the pwd
--- tests/test_single_qudit.py
+++ tests/test_single_qudit.py
@@ -4,44 +4,60 @@ import pennylane as qml
 from pennylane_ls import *
 import numpy as np
 
-class TestSingleQuditDevice(unittest.TestCase):
 
+class TestSingleQuditDevice(unittest.TestCase):
     def setUp(self):
-        self.username = 'synqs_test'
-        self.password = 'Cm2TXfRmXennMQ5'
-        self.testDevice = qml.device("synqs.sqs", shots = 50, username = self.username, password = self.password, blocking=True)
+        self.username = "synqs_test"
+        self.password = "Cm2TXfRmXennMQ5"
+        self.testDevice = qml.device(
+            "synqs.sqs",
+            shots=50,
+            username=self.username,
+            password=self.password,
+            blocking=True,
+        )
 
     def test_creation(self):
         testDevice = qml.device("synqs.sqs")
-        self.assertEqual(testDevice.operations, {'load', 'rLx', 'rLz', 'rLz2'})
+        self.assertEqual(testDevice.operations, {"load", "rLx", "rLz", "rLz2"})
 
     def test_creation_with_user(self):
-        '''
+        """
         Test creation with username
-        '''
-        testDevice = qml.device("synqs.sqs", shots = 50, username = self.username, password = self.password, blocking=True)
-        self.assertEqual(testDevice.operations, {'load', 'rLx', 'rLz', 'rLz2'})
+        """
+        testDevice = qml.device(
+            "synqs.sqs",
+            shots=50,
+            username=self.username,
+            password=self.password,
+            blocking=True,
+        )
+        self.assertEqual(testDevice.operations, {"load", "rLx", "rLz", "rLz2"})
 
     def test_load_gate(self):
-        '''
+        """
         Test the load gate
-        '''
+        """
+
         @qml.qnode(self.testDevice)
         def quantum_circuit():
-            load(50,wires=0)
-            rLx(np.pi,wires=0)
+            load(50, wires=0)
+            rLx(np.pi, wires=0)
             return qml.expval(SingleQuditOps.Z(0))
+
         res = quantum_circuit()
         self.assertEqual(int(res), 50)
 
     def test_rX_gate(self):
-        '''
+        """
         Test the rX gate
-        '''
+        """
+
         @qml.qnode(self.testDevice)
         def quantum_circuit():
-            load(50,wires=0)
-            rLx(np.pi,wires=0)
+            load(50, wires=0)
+            rLx(np.pi, wires=0)
             return qml.expval(SingleQuditOps.Z(0))
+
         res = quantum_circuit()
         self.assertEqual(int(res), 50)
