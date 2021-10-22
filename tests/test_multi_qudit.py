5bd341395a8aafbf066b4adc456edc7dfc384eb1
Fred Jendrzejewski <fred.jendrzejewski@gmail.com>
2021-10-22 07:53:57 +0200

    Update test_multi_qudit.py

--- tests/test_multi_qudit.py
+++ tests/test_multi_qudit.py
@@ -18,6 +18,9 @@ class TestMultiQuditDevice(unittest.TestCase):
         )
 
     def test_creation(self):
+        """
+        Test the creation of the Device
+        """
         testDevice = qml.device("synqs.mqs")
         self.assertEqual(
             testDevice.operations, {"LxLy", "LzLz", "load", "rLx", "rLz", "rLz2"}
