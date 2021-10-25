ce0f3f187afa11d6b34bbcf7bef817b41ab81dc9
fretchen <fred.jendrzejewski@gmail.com>
2021-10-25 19:45:13 +0200

    Got the multi-qudit system to be non-blocking (#41)
    * Got the system to be non-blocking
    
    * fixup: Format Python code with Black
    
    * Add the proper test for rLx rotation
    
    * Update test_multi_qudit.py
    
    Co-authored-by: autoblack <fretchen@users.noreply.github.com>

--- tests/test_multi_qudit.py
+++ tests/test_multi_qudit.py
@@ -1,11 +1,41 @@
 import unittest
 
 import pennylane as qml
+from pennylane_ls import *
+import numpy as np
 
 
 class TestMultiQuditDevice(unittest.TestCase):
+    def setUp(self):
+        self.username = "synqs_test"
+        self.password = "Cm2TXfRmXennMQ5"
+        self.testDevice = qml.device(
+            "synqs.mqs",
+            shots=50,
+            username=self.username,
+            password=self.password,
+            blocking=True,
+        )
+
     def test_creation(self):
+        """
+        Test the creation of the Device
+        """
         testDevice = qml.device("synqs.mqs")
         self.assertEqual(
             testDevice.operations, {"LxLy", "LzLz", "load", "rLx", "rLz", "rLz2"}
         )
+
+    def test_rX_gate(self):
+        """
+        Test the rX gate
+        """
+
+        @qml.qnode(self.testDevice)
+        def quantum_circuit():
+            load(50, wires=0)
+            rLx(np.pi, wires=0)
+            return qml.expval(SingleQuditOps.Z(0))
+
+        res = quantum_circuit()
+        self.assertEqual(int(res), 50)
