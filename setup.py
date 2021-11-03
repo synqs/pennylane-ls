8f4e9746df1fc2a4e074295442529181b8d3aacc
Fred Jendrzejewski <fred.jendrzejewski@gmail.com>
2021-11-03 07:34:00 +0100

    Update for release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ pennylane_devices_list = [
 
 setup(
     name="pennylane-ls",
-    version="0.2.0",
+    version="0.3.0",
     description="A Pennylane plugin for cold atom quantum simulators",
     url="https://www.github.com/synqs/pennylane-ls",
     author="Fred Jendrzejewski",
