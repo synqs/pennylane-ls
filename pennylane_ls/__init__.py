179490f5c5d73fd0613fdb782c998459f0b06943
fretchen <fred.jendrzejewski@gmail.com>
2022-01-26 18:18:40 +0100

    Update the notebooks and remove the old ops
    The ops where poorly overloaded anyways and are not tested. These imports might come back, when they are properly tested.

--- pennylane_ls/__init__.py
+++ pennylane_ls/__init__.py
@@ -1,12 +1,8 @@
 """
-The initialization of the pennylane-ls module
+The initialization of the `pennylane-ls` module
 """
 from .single_qudit_device import SingleQuditDevice
-from .single_qudit_ops import RLX, LZ, RLZ, RLZ2
-
 from .multi_qudit_device import MultiQuditDevice
-
-from .fermion_ops import Hop, Inter, Phase, ParticleNumber, PauliZ, HartreeFock
 from .fermion_device import FermionDevice
 
 from ._version import __version__
